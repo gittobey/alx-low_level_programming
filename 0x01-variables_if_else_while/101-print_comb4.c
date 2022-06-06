b4c6f8e25d80c24b025203f210815eb9d0aab81f
gittobey <kudowtobey@gmail.com>
2022-06-06 17:34:26 +0000

    submit 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,35 +8,36 @@
 */
 int main(void)
 {
-        int a = 0;
-        int b = 0;
+	int a = 0;
+	int b = 0;
 	int c = 0;
 
 	while (c < 10)
 	{
 		b = 1;
-	        while (b < 10)
-	        {
-	                a = 0;
-	                while (a < 10)
-	                {
-	                        if (a > b && b > c)
+		while (b < 10)
+		{
+			a = 0;
+			while (a < 10)
+			{
+				if (a > b && b > c)
 	                        {
 					putchar('0' + c);
-	                                putchar('0' + b);
-	                                putchar('0' + a);
-	                                if (a + b + c <= 23)
-	                                {
-	                                        putchar(',');
-	                                        putchar(' ');
-	                                }
-	                        }
-	                        a++;
-	                }
+					putchar('0' + b);
+					putchar('0' + a);
+					if (a + b + c <= 23)
+				       	{
+						putchar(',');
+						putchar(' ');
+			
+					}
+				}
+				a++;
+			}
 	                b++;
 		}
 		c++;
-        }
-        putchar('\n');
+	}
+	putchar('\n');
         return (0);
 }
