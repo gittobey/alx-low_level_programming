5efaf94149a08a8fb456d6b6f5ef952b45d2836a
gittobey <kudowtobey@gmail.com>
2022-06-07 22:15:57 +0000

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  * print_alphabet - print alphabets in lower case
+ *   *
+ *    * Return: void
+ *     */
+void print_alphabet_x10(void)
+{
+	int x;
+	int i;
+
+	for (x = 0; x < 3; x++)
+	{
+		for (i = 0; i < 26; i++)
+		{
+		_putchar(97 + i);
+		}
+	}
+	_putchar('\n');
+}
