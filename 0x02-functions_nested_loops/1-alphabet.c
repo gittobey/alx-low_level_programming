a9d5e8c9f13895bc428dce17947ab7a65ae32697
gittobey <kudowtobey@gmail.com>
2022-06-07 20:55:05 +0000

    print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+nclude "main.h"
+
+/**
+*  * print_alphabet - print alphabets in lower case
+*   *
+*    * Return: void
+*/
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 1; i < 27; i++)
+	{
+		_putchar(97 + i);
+	}
+	_putchar('\n');
+}
