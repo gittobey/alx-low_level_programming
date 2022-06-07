975b236e55a457d029e47868ca627ad11a79a33e
gittobey <kudowtobey@gmail.com>
2022-06-07 22:58:47 +0000

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/*
+ *checks for lower case
+ *returns 1 if lower case
+ *returns 0 if not
+*/
+int _islower(int c);
+{
+
+	if (c >= 97 && c <= 122)
+	{	
+		return (1);
+	else
+		return (0)
+}
