8ec46525b096b12c677d22aa2df23bbedb278470
gittobey <kudowtobey@gmail.com>
2022-06-08 00:26:07 +0000

    #include main.h

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *  * jack_bauer - print time from 00:00 to 23:59
+ *   *
+ *    * Return: void
+*/
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 2; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			if (a == 2 && b == 4)
+			return;
+			for (c = 0; c <= 5; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					_putchar('0' + a);
+					_putchar('0' + b);
+					_putchar(':');
+					_putchar('0' + c);
+					_putchar('0' + d);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
