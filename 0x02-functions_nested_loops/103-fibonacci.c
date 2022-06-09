2b67353d30786ad07fa487ae820322156d70ce03
gittobey <kudowtobey@gmail.com>
2022-06-09 05:00:26 +0000

    advanced task update

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*  * main - cumputes sum of all multiples of 3 and 5 below 1024
+**
+*   *
+*    * Return: void
+*/
+int main(void)
+{
+	int i;
+	int total;
+
+	total = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+				total = total + i;
+	}
+	printf("%d\n", total);
+
+	return (0);
+}
