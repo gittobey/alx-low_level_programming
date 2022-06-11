f589b9509f04d9e1c40e576e5db63207fdc7953f
gittobey <kudowtobey@gmail.com>
2022-06-11 00:11:21 +0000

    check 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,12 +6,13 @@
  * @c: letter to be tested
  * Return: 1 if c is a digit, 0 otherwise
 */
+
 int _isdigit(int c)
 {
-        if (c <= 57 && c >= 48)
-        {
-                return (1);
-        }
-        else
-                return (0);
+	if (c <= 57 && c >= 48)
+	{
+		return (1);
+	}
+	else
+		return (0);
 }
