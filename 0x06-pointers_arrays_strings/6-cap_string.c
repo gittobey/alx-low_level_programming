579b048d5280413e21f63fb227ca64a6740010ec
gittobey <kudowtobey@gmail.com>
2022-06-16 22:08:23 +0000

    6-corrected

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,18 +5,38 @@
  * @s: The string input
  * Return: void
 */
-char *cap_string(char *s)
+char *cap_string(char *str)
 {
-	int i;
+	char sep[] = ",\t;\n; .!?\"(){}";
+	int flag, i, ii;
 
-	i = 0;
-
-	while (*(s + i) != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (*(s + i) >= 97 && *(s + i) <= 122 &&
-				(*(s + i - 1) <= 40))
-			*(s + i) = *(s + i) - 32;
-		i++;
+		flag = 0;
+
+		if (i == 0)
+		{
+			flag = 1;
+		}
+		else
+		{
+			for (ii = 0; sep[ii] != '\0'; ii++)
+			{
+				if (str[i - 1] == sep[ii])
+				{
+					flag = 1;
+					break;
+				}
+			}
+		}
+
+		if (flag == 1)
+		{
+			if (str[i] <= 'z' && str[i] >= 'a')
+			{
+				str[i] -= ('a' - 'A');
+			}
+		}
 	}
-	return (s);
+return (str);
 }
