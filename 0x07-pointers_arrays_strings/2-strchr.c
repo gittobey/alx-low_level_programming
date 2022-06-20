ed53b74c28ac93e4c9d70d6231e78270ed287979
gittobey <kudowtobey@gmail.com>
2022-06-20 22:01:59 +0000

    a function that fills memory with a constant byte.

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strchr -  a function that locates a character in a string
+ *
+ * @s: The string
+ * @c: The character we are searching for
+ * Return: Pointer to the char in the string s
+*/
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			break;
+	} while (*s++);
+
+	return (s);
+}
