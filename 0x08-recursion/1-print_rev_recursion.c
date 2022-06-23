bd12ae340d19f969adce5c56183d0152bb9eeb02
gittobey <kudowtobey@gmail.com>
2022-06-23 04:28:53 +0000

    1- corrected

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * _print_rev_recursion - A function using recursion to reverse a string.
- * 
+ *
  * @s: string input
  *
  * Return: Void
@@ -10,7 +10,7 @@
 void _print_rev_recursion(char *s)
 {
 	if (*s)
-	{   
+	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
