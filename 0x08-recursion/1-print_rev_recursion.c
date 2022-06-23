c107399f0d856faf55fbee01cf35e3cabcaa118f
gittobey <kudowtobey@gmail.com>
2022-06-23 04:21:20 +0000

    C - Recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - A function using recursion to reverse a string.
+ * 
+ * @s: string input
+ *
+ * Return: Void
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{   
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+/**
+ * _strlen_recursion - A function using recursion to find length of
+ * string.
+ *
+ * @s: string input
+ * Return: length of string (int)
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
