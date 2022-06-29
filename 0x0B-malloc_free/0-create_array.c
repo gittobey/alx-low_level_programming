8186b3f376d9d4dff157135468604d91bf81c6de
gittobey <kudowtobey@gmail.com>
2022-06-29 14:30:47 +0000

    memory exercises

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - a function that creates an array of chars, and
+ * initializes it with a specific char
+ *
+ * @size: size of array
+ * @c: character input
+ * Return: void
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	ptr = malloc(sizeof(char) * size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		ptr[i] = c;
+	ptr[i] = '\0';
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,3 @@
+#Memory Allocation
+
+In this project, we explore the malloc, and free function to Dynamically allocate memory to our variables and Arrays.
