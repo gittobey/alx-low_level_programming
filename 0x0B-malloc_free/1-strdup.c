ae398783062c581af7d06fec815f9cc0469fd37b
gittobey <kudowtobey@gmail.com>
2022-06-30 00:35:31 +0000

    1-corrected

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,57 +1,33 @@
-#include "main.h"
 #include <stdlib.h>
-/**
- * _strlen - get the length of a string
- *
- * @s: string input
- * Return: Length in int
- */
-int _strlen(char *s)
-{
-	int len;
+#include "main.h"
 
+/**
+ * *_strdup - copies the string given as parameter
+ * @str: string to duplicate
+ *
+ * Return: pointer to the copied string (Success), NULL (Error)
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	unsigned int i, len;
+
+	i = 0;
 	len = 0;
-
-	while (*(s + len) != '\0')
-		len++;
-
-	return (len);
-}
-
-/**
- * str_concat - a function that concatenates two strings
- *
- * @s1:  string1
- * @s2: string2
- * Return: string
- */
-char *str_concat(char *s1, char *s2)
-{
-	int i, len1, len2;
-	char *str;
-
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-
-	str = malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (str == NULL)
 		return (NULL);
 
-	for (i = 0; i < (len1 + len2); i++)
-	{
-		if (i < len1)
-			*(str + i) = s1[i];
-		else
-			*(str + i) = s2[i - len1];
-	}
+	while (str[len])
+		len++;
 
-	*(str + i + 1) = '\0';
+	dup = malloc(sizeof(char) * (len + 1));
 
-	return (str);
+	if (dup == NULL)
+		return (NULL);
+
+	while ((dup[i] = str[i]) != '\0')
+		i++;
+
+	return (dup);
 }
